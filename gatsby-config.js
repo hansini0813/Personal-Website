5b82056eb3c19606771d104b8f82f44e51a56ba7
Hansini Mirchandani <mirchandanihansini@gmail.com>
2024-09-15 22:34:40 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `hansinimirchandani`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
